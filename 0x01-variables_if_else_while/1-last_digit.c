247d2acdb02309cc2f0d937c98d65102816333ec
Avalanche100 <akpanblessing18@gmail.com>
2022-03-14 18:08:57 -0700

    Pirnt the lastn

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -29,7 +29,7 @@ printf("Last digit of %d is %d and is 0\n", n, lastn);
 }
 else if (lastn < 6 && lastn != 0)
 {
-printf("Lasr digit of %d is %d and is less than 6 and not 0\n", n, lastn);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastn);
 }
 return (0);
 }
