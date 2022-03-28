e050b0efbd27094bb18629c97187b3663e8e84bc
Sabiduria10 <oyinseyeshola@gmail.com>
2022-03-28 17:46:51 +0100

    more nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - program that adds two integers and returns the result.
+ * @k1: input integer 1
+ * @k2: input integer 2
+ * Return: nothing.
+ */
+
+int add(int k1, int k2)
+{
+	int bless;
+
+	bless = k1 + k2;
+	return (bless);
+}
+
