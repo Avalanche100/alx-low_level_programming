14338d1194d32ef97929aadbc7e7b05c6a7744bf
Sabiduria10 <oyinseyeshola@gmail.com>
2022-03-28 18:32:50 +0100

    more nested loops

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - program that finds and prints the sum of the 
+ * even-valued terms, followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int k = 1, l = 2, total = 0;
+	int z;
+
+	while (l < 4000000)
+	{
+		if (l % 2 == 0)
+			total += l;
+
+		z = l;
+		l += k;
+		k = z;
+	}
+	printf("%d\n, total);
+	return (0);
+}
+
