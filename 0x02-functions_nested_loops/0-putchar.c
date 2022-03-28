cc7737f12689c980151a561db58b0129d6ea35f0
Sabiduria10 <oyinseyeshola@gmail.com>
2022-03-28 10:45:31 +0100

    more nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/** main - a program that prints "_putchar" followed by a new line.
+ * Description - You are not allowed to use standard libraries.
+ * Return: 0
+ */
+int main(void)
+{
+	char sabi[] = "_putchar",
+	     int a = 0;
+
+	while (sabi[a] != '\0')
+	{
+		_putchar(sabi[a]);
+		a++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
